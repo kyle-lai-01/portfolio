e959878aa1440f7dc7b54e420d603194bc0e2f04
Kyle Lai <kl655@cornell.edu>
2020-08-15 21:35:35 -0400

    Changed app files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
+
 
 function App() {
+
+
+
+  const showMsg = () => 'Hello world'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '100%',
+        height: "100vh"
+      }}
+    >
+      <Terminal
+        color='green'
+        backgroundColor='black'
+        outputColor='white'
+        barColor='lightgrey'
+        allowTabs={false}
+        startState='maximised'
+        style={{ fontWeight: "bold", fontSize: "1em", width: '100%' }}
+        commands={{
+          'open-google': () => window.open('https://www.google.com/', '_blank'),
+          showmsg: showMsg,
+          popup: () => alert('Terminal in React')
+        }}
+        descriptions={{
+          'open-google': 'opens google.com',
+          showmsg: 'shows a message',
+          alert: 'alert', popup: 'alert'
+        }}
+        msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+      />
     </div>
   );
 }
